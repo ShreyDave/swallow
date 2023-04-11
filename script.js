a706d8c9b277ae49cd120cf3d47e224521608ec3
ShreyDave <shreykdave2411@gmail.com>
2023-04-11 10:47:44 +0530

    update code

--- script.js
+++ script.js
@@ -1,11 +1,58 @@
+// $(document).ready(function () {
+//     $('.slider').slick({
+//         autoplay: true,
+//         autoplaySpeed: 750,
+//         dots: false,
+//         infinite: true,
+//         speed: 750,
+//         slidesToShow: 4,
+//         slidesToScroll: 1,
+//     });
+// });
+
+
 $(document).ready(function () {
     $('.slider').slick({
-        autoplay: true,
+        autoplay: false,
         autoplaySpeed: 750,
-        dots: false,
-        infinite: true,
-        speed: 750,
         slidesToShow: 4,
         slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        speed: 750,
+        infinite: true,
+        responsive: [
+            {
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 4,
+                }
+            },
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 760,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+        ]
+    });
+});
+
+$(document).ready(function () {
+    $(".menuIcon").on("click", function () {
+        $(this).toggleClass("active");
+        $(".menubar").toggleClass("active");
     });
 });
