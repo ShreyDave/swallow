6bcb8880805cdde0d3e7bba7ececd2610514955e
ShreyDave <shreykdave2411@gmail.com>
2023-04-17 18:47:28 +0530

    add code

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-// $(document).ready(function () {
-//     $('.slider').slick({
-//         autoplay: true,
-//         autoplaySpeed: 750,
-//         dots: false,
-//         infinite: true,
-//         speed: 750,
-//         slidesToShow: 4,
-//         slidesToScroll: 1,
-//     });
-// });
-
-
 $(document).ready(function () {
     $('.slider').slick({
         autoplay: false,
