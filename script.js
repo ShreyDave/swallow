cfde4a7a591bfa43cfd55f377fe34bf4a138d10e
ShreyDave <shreykdave2411@gmail.com>
2023-04-19 18:43:41 +0530

    add code

--- script.js
+++ script.js
@@ -10,7 +10,40 @@ $(document).ready(function () {
         infinite: true,
         responsive: [
             {
-                breakpoint: 1400,
+                breakpoint: 1400,   
+                settings: {
+                    slidesToShow: 4,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 577,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+        ]
+    });
+});
+
+$(document).ready(function () {
+    $('.slider1').slick({
+        autoplay: true,
+        autoplaySpeed: 750,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        speed: 750,
+        infinite: true,
+        responsive: [
+            {
+                breakpoint: 1400,   
                 settings: {
                     slidesToShow: 4,
                 }
