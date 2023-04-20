b9726ed8e7fb480a77e58345245bc9997df48d8f
ShreyDave <shreykdave2411@gmail.com>
2023-04-20 12:56:55 +0530

    add code

--- script.js
+++ script.js
@@ -64,6 +64,40 @@ $(document).ready(function () {
     });
 });
 
+
+$(document).ready(function () {
+    $('.slider2').slick({
+        autoplay: true,
+        autoplaySpeed: 750,
+        slidesToShow: 2.19,
+        slidesToScroll: 0.8,
+        arrows: true,
+        dots: false,
+        speed: 750,
+        infinite: true,
+        responsive: [
+            {
+                breakpoint: 1400,   
+                settings: {
+                    slidesToShow: 4,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 577,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+        ]
+    });
+});
+
 $(document).ready(function () {
     $(".menuIcon").on("click", function () {
         $(this).toggleClass("active");
